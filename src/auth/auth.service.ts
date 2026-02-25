7b0b915665f9a055a05e9d352e00da3ff29083fb
obedeDintala123 <obededintala@gmail.com>
2026-02-25 20:53:00 +0100

    fix: error type

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -69,7 +69,7 @@ export class AuthService {
     response.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
