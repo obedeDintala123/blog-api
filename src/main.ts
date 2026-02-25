c0368deca383e05924b6fbf03413ce7aec7f4cc5
obedeDintala123 <obededintala@gmail.com>
2026-02-25 20:41:19 +0100

    fix: error type

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONT_URL ?? 'http://localhost:3000',
+    origin: process.env.FRONT_URL,
     credentials: true,
   });
 
